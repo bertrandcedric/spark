00479a4776bc8c59454c73115a71222007b6bbe1
CEDRIC BERTRAND <cedric-externe.bertrand@erdf.fr>
2016-02-01 16:41:55 +0100

    Spark

--- src/main/scala/SparkRandomForest.scala
+++ src/main/scala/SparkRandomForest.scala
@@ -1,8 +1,11 @@
 import java.io.{File, PrintWriter}
 
+import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, SVMWithSGD}
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.{RandomForest, GradientBoostedTrees}
+import org.apache.spark.mllib.tree.configuration.Algo.Classification
+import org.apache.spark.mllib.tree.configuration.BoostingStrategy
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkRandomForest {
@@ -21,37 +24,40 @@ object SparkRandomForest {
 
     //load train data
     val data = sc.textFile("data/train.csv")
-    val transformedData = data.map(d => {
+    val splits = data.randomSplit(Array(0.98, 0.02))
+    val (trainingData, checkData) = (splits(0), splits(1))
+
+    val train = trainingData.map(d => {
       val l = d.replaceAll(";", ",").split(",")
-      LabeledPoint(l.head.toDouble, new DenseVector(l.tail.map(s => mapTo1(s.toDouble))))
+      LabeledPoint(l.head.toInt, new DenseVector(l.tail.map(s => mapTo1(s.toInt))))
     })
 
-    val numClasses = 10
-    val categoricalFeaturesInfo = Map[Int, Int]()
-    val numTrees = 25 // Use more in practice.
-    val featureSubsetStrategy = "auto" // Let the algorithm choose.
-    val impurity = "gini"
-    val maxDepth = 4
-    val maxBins = 100
+//    val numClasses = 10
+//    val categoricalFeaturesInfo = Map[Int, Int]()
+//    val numTrees = 25 // Use more in practice.
+//    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+//    val impurity = "gini"
+//    val maxDepth = 4
+//    val maxBins = 100
+//    val model = RandomForest.trainClassifier(
+//      train,
+//      numClasses,
+//      categoricalFeaturesInfo,
+//      numTrees,
+//      featureSubsetStrategy,
+//      impurity,
+//      maxDepth,
+//      maxBins,
+//      0)
 
-    val model = RandomForest.trainClassifier(
-      transformedData,
-      numClasses,
-      categoricalFeaturesInfo,
-      numTrees,
-      featureSubsetStrategy,
-      impurity,
-      maxDepth,
-      maxBins,
-      0)
-
-    println("Learned classification forest model:\n" + model.toDebugString)
+    val model = new LogisticRegressionWithLBFGS()
+      .setNumClasses(10)
+      .run(train)
 
     // Verification des labels avec un sous ensemble de données
-    val dataCopie = sc.textFile("data/trainCopie.csv")
-    val transformedCopieData = dataCopie.map(d => {
+    val transformedCopieData = checkData.map(d => {
       val l = d.replaceAll(";", ",").split(",")
-      LabeledPoint(l.head.toDouble, new DenseVector(l.tail.map(s => mapTo1(s.toDouble))))
+      LabeledPoint(l.head.toInt, new DenseVector(l.tail.map(s => mapTo1(s.toInt))))
     })
 
     val checkLabel = transformedCopieData.map { point =>
@@ -60,17 +66,17 @@ object SparkRandomForest {
     }
 
     checkLabel.collect().foreach(println)
-    val testErr = checkLabel.filter(r => r._1 != r._2).count.toDouble / dataCopie.count()
+    val testErr = checkLabel.filter(r => r._1 != r._2).count.toDouble / checkData.count()
     println("Test Error = " + testErr)
 
     // Prediction avec le jeu de test
     val testData = sc.textFile("data/test.csv")
-    val transformedTestData = testData.map(d => {
-      val l = d.replaceAll(";", ",").split(",").map(s => mapTo1(s.toDouble))
+    val test = testData.map(d => {
+      val l = d.replaceAll(";", ",").split(",").map(s => mapTo1(s.toInt))
       new DenseVector(l)
     })
 
-    val prediction = transformedTestData.map { point =>
+    val prediction = test.map { point =>
       model.predict(point)
     }
 
