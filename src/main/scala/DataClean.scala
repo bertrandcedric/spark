54d0d643faa2631c2a055779e88b6530069caa68
Fabrice Sznajderman <fabrice-externe.snzajderman@edf.fr>
2016-02-04 14:28:01 +0100

    compile data

---
+++ src/main/scala/DataClean.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+/**
+  * Created by fs24818n on 03/02/2016.
+  */
+object DataClean extends App {
+
+
+  val train: Iterator[String] = Source.fromFile("data/train.csv").getLines()
+  val all = train.flatMap(f => f.split(",").map(d => d.toInt).filter(e => e != 0)).toSeq
+
+  //println(all.sum)
+
+
+
+  println(all.sum / all.size)
+
+
+  //println(all.sum / all.size)
+
+
+}
